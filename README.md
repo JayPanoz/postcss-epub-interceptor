5942a1ced07d68fe3229ea7228a27aa814a0c0ea
JayPanoz <panozpublishingteam@gmail.com>
2017-10-23 16:26:49 +0200

    Init plugin

--- README.md
+++ README.md
@@ -1,2 +1,102 @@
 # postcss-epub-interceptor
-A PostCSS plugin to unprefix EPUB properties
+
+A [PostCSS](https://github.com/postcss/postcss) plugin to unprefix EPUB3 properties.
+
+See [EPUB 3.1 Mapping](http://www.idpf.org/epub/31/spec/epub-contentdocs.html#sec-css-prefixed) for further details.
+
+## Warning
+
+This is very alpha (v.0.1) and is more of a proof of concept than an elegant plugin. It gets the job done but is neither elegant nor optimized.
+
+At least it implements the correct mapping for `-epub-properties` back to EPUB 3.0.0 – I just needed to do a practical implementation in order to get all the details and will redo it from scratch ASAP. In the meantime, please feel free to fork it, improve code and pull request.
+
+## Usage
+
+```js
+
+postcss([ require("postcss-epub-interceptor") ])
+
+// postcss.config.js
+
+module.exports = (ctx) => ({
+  plugins: [
+    require('postcss-epub-interceptor')({ })
+  ]
+})
+```
+
+## Prefixing
+
+If you need to prefix those properties, please use [autoprefixer](https://github.com/postcss/autoprefixer). 
+
+## Example
+
+### Input
+
+```css
+
+.text-1 {
+	-epub-hyphens: auto;
+	-epub-line-break: normal;
+	-epub-text-align-last: center;
+	-epub-word-break: break-all;
+	text-transform: -epub-fullsize-kana; 
+}
+
+.text-2 {
+	-epub-hyphens: none;
+	hyphens: none;
+	text-align-last: center;
+	-epub-word-break: break-all;
+	word-break: break-all;
+  text-transform: -epub-fullwidth;	
+}
+
+.text-3 {
+	text-transform: -epub-fullwidth;
+	text-transform: full-width;
+}
+```
+
+### Output
+
+```css
+
+.text-1 {
+	-epub-hyphens: auto;
+	hyphens: auto;
+	-epub-line-break: normal;
+	line-break: normal;
+	-epub-text-align-last: center;
+	text-align-last: center;
+	-epub-word-break: break-all;
+	word-break: break-all;
+	text-transform: -epub-fullsize-kana; 
+}
+
+.text-2 {
+	-epub-hyphens: none;
+	hyphens: none;
+	text-align-last: center;
+	-epub-word-break: break-all;
+	word-break: break-all;
+  text-transform: -epub-fullwidth;
+  text-transform: full-width;	
+}
+
+.text-3 {
+	text-transform: -epub-fullwidth;
+	text-transform: full-width;
+}
+```
+
+## Notes
+
+The following declarations are not supported: 
+
+- `text-transform: -epub-fullsize-kana` (CSS Text Level 3 module has dropped support for the `fullsize-kana` value);
+- `-epub-text-combine: horizontal <digit>`.
+
+## License
+
+MIT
---
+++ index.js
@@ -0,0 +1,121 @@
+"use strict";
+
+var postcss = require("postcss"),
+
+		/* Properties to check */
+		epubProps = [
+			/* epub text */
+			"-epub-hyphens",
+			"-epub-line-break",
+			"-epub-text-align-last",
+			"-epub-word-break",
+
+			/* epub text decoration */
+			"-epub-text-emphasis",
+			"-epub-text-emphasis-color",
+			"-epub-text-emphasis-style",
+			"-epub-text-emphasis-position",
+			"-epub-text-underline-position",
+			
+			/* epub writing modes */
+			"-epub-writing-mode",
+			"-epub-text-orientation",
+			"-epub-text-combine",
+			"-epub-text-combine-horizontal",
+			"-epub-text-combine-upright",
+
+			/* */
+			"epub-ruby-position"
+		],
+
+		/* For some props, we need to change the prop/value as well */
+		mappings = [
+			{epubProp: "-epub-text-underline-position", epubVal: "alphabetic", stdVal: "auto"},
+			{epubProp: "-epub-text-orientation", epubVal: "vertical-right", stdVal: "mixed"},
+			{epubProp: "-epub-text-orientation", epubVal: "sideways-right", stdVal: "sideways"},
+			{epubProp: "-epub-text-orientation", epubVal: "rotate-right", stdVal: "sidaways"},
+			{epubProp: "-epub-text-orientation", epubVal: "rotate-normal", stdVal: "sideways"},
+			{epubProp: "-epub-text-combine", epubVal: "horizontal", stdProp: "text-combine-upright", stdVal: "all"},
+			{epubProp: "-epub-text-combine-horizontal", epubVal: "all", stdProp: "text-combine-upright"}
+		];
+
+module.exports = postcss.plugin("postcss-epub-interceptor", function(opts) {
+	opts = opts || {};
+
+	/**
+	 * PostCSS plugin to unprefix ePub3 properties
+	 * @param {Object} css
+	 */
+
+	return function(css) {
+		css.walkDecls(function(decl) {
+			if (decl.value) {
+
+				/* If text-transform */
+				if (decl.value === "-epub-fullwidth" && decl.prop === "text-transform") {
+					if (decl.parent.some(x => x.prop !== "text-transform" && x.value !== "full-width")) {
+						decl.cloneAfter({
+							prop: "text-transform",
+							value: "full-width"
+						});
+					}
+				}
+				
+				/* If declaration property in the list */
+				 else if (epubProps.indexOf(decl.prop) >= 0) {
+
+					/* Check if the unprefixed property already exists */					
+					if (decl.prop === "-epub-text-combine-horizontal" || decl.prop === "-epub-text-combine") {
+						var unprefixedProp = "text-combine-upright";
+					} else {
+						var unprefixedProp = decl.prop.replace(/-epub-/i, "");
+					}
+
+					var hasUnprefixed = decl.parent.some(y => y.prop === unprefixedProp);
+
+					/* If the unprefixed prop doesn’t exist */
+					if (!hasUnprefixed) {
+
+						/* Check if we have to change the value for this prop as well */
+						var idx = mappings.findIndex(z => z.epubVal === decl.value && z.epubProp === decl.prop);
+					
+						/* If we have to change the value and/or prop */
+						if (idx >= 0) {
+
+							/* If we have to change the prop */
+							if (mappings[idx].stdProp) {
+								
+								/* And the value as well */
+								if (mappings[idx].stdVal) {
+									decl.cloneAfter({
+										prop: mappings[idx].stdProp,
+										value: mappings[idx].stdVal
+									});
+								} else {
+									decl.cloneAfter({
+										prop: mappings[idx].stdProp
+									});
+								}
+							} else {
+
+							/* Clone the epub-prop to add the unprefixed one and the new value */
+								decl.cloneAfter({
+									prop: unprefixedProp,
+									value: mappings[idx].stdVal
+								});
+							}
+						} else {
+
+							/* Clone the epub-prop to add the unprefixed one */
+							decl.cloneAfter({
+								prop: unprefixedProp
+							});
+						}
+					}
+				}
+			}
+		});
+	};
+});
+
+// If you need to prefix properties, use the autoprefixer plugin
